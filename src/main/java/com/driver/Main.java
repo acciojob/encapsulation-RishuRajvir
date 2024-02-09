ac0a2f56461f5819deea217d6c8ec3e49b4aed1f
Rishu Raj <106235785+RishuRajvir@users.noreply.github.com>
2024-02-10 00:12:31 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
             RWOnly r = new RWOnly();
-            r.setname("Rishu Raj");
-            r.setroll(23);
-        System.out.println(r.getname()+" "+r.getroll());
+            r.setter("Rishu Raj");
+            // r.g(23);
+        System.out.println(r.getname());
     }
 }
