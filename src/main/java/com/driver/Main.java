bd5ccf2de08acbc0c796d2eda422b3bf0d7e110a
Rishu Raj <rishu25dec@gmail.com>
2024-02-09 16:50:32 +0530

    I write the Concept of Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+            RWOnly r = new RWOnly();
+            r.setname("Rishu Raj");
+            r.setroll(23);
+        System.out.println(r.getname()+" "+r.getroll());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private   int roll;
+    private String name;
+//    public RWOnly(int roll, String name){
+//        this.roll = roll;
+//        this.name = name;
+//    }
+    public void setroll(int roll){
+        if(roll<0)
+            return;
+        this.roll = roll;
+    }
+    public int getroll(){
+        return this.roll;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+    public String getname(){
+        return this.name;
+    }
 }
